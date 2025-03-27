1ad5325696889290cc83a673932f9f0b985d77dc
Nani-REON <nani.sunoflife@gmail.com>
2025-03-27 04:11:52 +0330

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,19 +1,16 @@
-
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const path = require("path");
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// صفحه اصلی
 app.get("/", (req, res) => {
   res.send("REON Webhook is live and listening.");
 });
 
-// مسیر نمایش فرم
 app.get("/form", (req, res) => {
   const formPath = path.join(__dirname, "reon_contact_form.html");
   fs.readFile(formPath, "utf8", (err, data) => {
@@ -25,14 +22,12 @@ app.get("/form", (req, res) => {
   });
 });
 
-// نقطه دریافت پیام از فرم
 app.post("/webhook", (req, res) => {
-  console.log("Received webhook:", req.body);
-  res.send({ status: "received", data: req.body });
+  console.log("RECEIVED:", req.body);
+  res.send({ status: "success", received: req.body });
 });
 
-// اجرای سرور
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log("REON webhook server is running on port", port);
+  console.log("Server running on port", port);
 });
